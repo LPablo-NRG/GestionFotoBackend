adbedb9651af4789a0c5f5654114182d87c99b32
luisp <luisp@TAKISZOMBI>
2025-03-03 20:16:02 -0600

    Resolviendo errores

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const PhotoSchema = new mongoose.Schema({
-userId: { type: String, required: true }, // Usuario propietario de la
-foto
+userId: { type: String, required: true }, // Usuario propietario de la foto
 imageUrl: { type: String, required: true }, // URL de la imagen subida
 createdAt: { type: Date, default: Date.now },
 });
--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static("uploads")); // Servir imágenes
-estáticamente
+app.use("/uploads", express.static("uploads")); // Servir imágenes estáticamente
 // Conexión a MongoDB
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 
 mongoose
 .connect(process.env.MONGO_URI, { useNewUrlParser: true,
