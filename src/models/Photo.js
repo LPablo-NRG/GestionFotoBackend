c2aa8d46f2d98ef96cc616dc8e724aefa586b022
luisp <luisp@TAKISZOMBI>
2025-03-03 18:36:53 -0600

    Creación del modelo de Datos

---
+++ src/models/Photo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const PhotoSchema = new mongoose.Schema({
+userId: { type: String, required: true }, // Usuario propietario de la
+foto
+imageUrl: { type: String, required: true }, // URL de la imagen subida
+createdAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Photo", PhotoSchema);
